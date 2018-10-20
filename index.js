f1b500c9705970a1b3b4f6c6fdc52ea55efebebf
zestian56 <sebas-565@hotmail.com>
2018-10-19 22:08:40 -0500

    Correciones de espaciado y margenes

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-var modal = document.getElementById('modal');
-var modal_content =document.getElementById('modal-content');
+const elModal = document.getElementById('modal');
+const elModalContent = document.getElementById('modal-content');
 
 
-function displayModal(){
-    modal.style.display = 'flex';
-    modal_content.style.top = '0px';
+function displayModal() {
+    elModal.classList.add('active');
 }
 
 function sendMessage() {
-    const objEmail  = {
-        stNombre :document.getElementById("txtNombre").value,
-        stCorreo : document.getElementById("txtCorreo").value,
+    const objEmail = {
+        stNombre: document.getElementById("txtNombre").value,
+        stCorreo: document.getElementById("txtCorreo").value,
         stMensaje: document.getElementById("txtMensaje").value
     }
-    
+
     console.log(objEmail)
 }
 
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal_content.style.top = '-1000px';
-        modal.style.display = "none";
+    if (event.target == elModal) {
+        elModal.classList.remove('active');
     }
 }
